c6b128ca342265d0c52b11a5ea0e8d3bd462a4b5
lexfernandez <olorenzo@outlook.com>
2019-01-26 12:07:59 -0600

    heroes
    1. The Hero Editor
    2. Displaying a List
    3. Master/Detail Components
    4. Services

---
+++ src/app/hero.service.ts
@@ -0,0 +1,17 @@
+import { MessageService } from './message.service';
+import { HEROES } from './mock-heroes';
+import { Hero } from './hero';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: Fetched heroes');
+    return of(HEROES);
+  }
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,4 @@
-import { HEROES } from './../mock-heroes';
+import { HeroService } from './../hero.service';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
 
@@ -10,12 +10,19 @@ import { Hero } from '../hero'
 })
 
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) {
+
+  }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero): void {
